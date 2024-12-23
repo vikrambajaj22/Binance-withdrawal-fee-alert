490e69942a3dd9c5ae9d2539f313e57a1f3b1754
Vikram Bajaj <vikrambajaj@nyu.edu>
2024-12-22 19:08:04 -0500

    schedule

--- get_withdrawal_fee.py
+++ get_withdrawal_fee.py
@@ -2,23 +2,36 @@ from binance.client import Client
 import schedule
 import time
 
-def get_withdrawal_fee(coin_symbol="DOGE"):
+def get_withdrawal_fee(coin_symbol="DOGE", chosen_network="DOGE"):
     try:
         price = float(client.get_symbol_ticker(symbol=coin_symbol+"USDT")["price"])
         all_coins_info = client.get_all_coins_info()
+        coin_details = {}
         for coin in all_coins_info:
             if coin["coin"] == coin_symbol:
                 networks = coin["networkList"]
                 for network in networks:
-                    print(f"network: {network['network']}, fee: {network['withdrawFee']}, fee_value: ${float(network['withdrawFee'])*price} @ ${price}, withdraw_min: {network['withdrawMin']}, , withdraw_max: {network['withdrawMax']}")
+                    if network["network"] == chosen_network:
+                        coin_details = {
+                            "network": network["network"],
+                            "fee": network["withdrawFee"],
+                            "usdt_price": price,
+                            "fee_value": float(network["withdrawFee"]) * price,
+                            "withdraw_min": network["withdrawMin"],
+                            "withdraw_max": network["withdrawMax"]
+                        }
+                        break
                 break
+        return coin_details
     except Exception as e:
         print(f"An error occurred: {repr(e)}")
 
 def job():
-    coin_symbols = ["DOGE", "BTC", "SHIB"]
-    for symbol in coin_symbols:
-        get_withdrawal_fee(symbol)
+    coin_symbols = {"DOGE": "DOGE", "BTC": "BTC", "SHIB": "ETH"}  # symbol:network
+    coin_details = {}
+    for symbol, network in coin_symbols.items():
+        coin_details[symbol] = get_withdrawal_fee(symbol, network)
+    print(coin_details)
 
 if __name__ == "__main__":
     with open("api-access-key.txt", "r") as f:
@@ -28,7 +41,7 @@ if __name__ == "__main__":
 
     client = Client(api_key, api_secret, tld="us")
 
-    schedule.every(30).minutes.do(job)
+    schedule.every(1).hour.do(job)
 
     while True:
         schedule.run_pending()
