eae16ebf900daedb3d3b416a7e06616a9642ec47
Vikram Bajaj <vikrambajaj@nyu.edu>
2024-12-25 15:53:56 -0500

    init client on top

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from get_withdrawal_fee import get_withdrawal_fee
 
 app = Flask(__name__)
 
-global client
+api_key = os.getenv("BINANCE_API_ACCESS_KEY", "")
+api_secret = os.getenv("BINANCE_API_SECRET_KEY", "")
+client = Client(api_key, api_secret, tld="us")
 
 @app.route("/")
 def index():
@@ -18,7 +20,6 @@ def index():
     return render_template("index.html", data=data)
 
 def job():
-    global client
     # each coin symbol has a tuple with network, threshold fee, and threshold fee_value
     coin_symbols = {"DOGE": ("DOGE", 50, 10), "BTC": ("BTC", 0.0004, 20), "SHIB": ("ETH", 100000, 2)}
     coin_details = {}
@@ -27,10 +28,6 @@ def job():
     return coin_details
 
 if __name__ == "__main__":
-    api_key = os.getenv("BINANCE_API_ACCESS_KEY", "")
-    api_secret = os.getenv("BINANCE_API_SECRET_KEY", "")
-    global client
-    client = Client(api_key, api_secret, tld="us")
     app.run(host=os.getenv("FLASK_HOST"), port=os.getenv("FLASK_PORT"), debug=True)
     schedule.every(30).minutes.do(job)
     while True:
